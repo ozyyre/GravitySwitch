2b22824cd16ae47e81dbbc590944279bc8002a85
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-14 21:06:03 +0100

    Refactor Obstacle class to randomize vertical position

--- classes/Obstacle.js
+++ classes/Obstacle.js
@@ -1,35 +1,41 @@
 class Obstacle {
-    constructor() {
-        this.size = random(30, 60);
-        this.x = width + this.size;
-        this.y = random(this.size/2, height - this.size/2);
-        this.speed = 5;
-        this.angle = random(TWO_PI);   // počáteční úhel
-        this.rotationSpeed = random(-0.05, 0.05); // rychlost rotace
+  constructor() {
+    this.size = random(30, 60);
+    this.x = width + this.size;
+    
+    if (random(1) < 0.5) {
+      this.y = random(this.size, height / 3);
+    } else {
+      this.y = random(2 * height / 3, height - this.size);
     }
 
-    update() {
-        this.x -= this.speed;
-        this.angle += this.rotationSpeed;
-    }
+    this.speed = 5;
+    this.angle = random(TWO_PI);
+    this.rotationSpeed = random(-0.05, 0.05);
+  }
 
-    show() {
-        push();
-        translate(this.x, this.y);
-        rotate(this.angle);
-        imageMode(CENTER);
-        image(meteorImg, 0, 0, this.size, this.size);
-        pop();
-    }
+  update() {
+    this.x = this.x - this.speed;
+    this.angle = this.angle + this.rotationSpeed;
+  }
 
-    offscreen() {
-        return this.x + this.size/2 < 0;
-    }
+  show() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    imageMode(CENTER);
+    image(meteorImg, 0, 0, this.size, this.size);
+    pop();
+  }
 
-    hits(player) {
-        const rect = player.getRect();
-        const closestX = constrain(rect.x, this.x - this.size/2, this.x + this.size/2);
-        const closestY = constrain(rect.y, this.y - this.size/2, this.y + this.size/2);
-        return dist(rect.x, rect.y, closestX, closestY) < this.size/2;
-    }
+  offscreen() {
+    return this.x + this.size / 2 < 0;
+  }
+
+  hits(player) {
+    let rect = player.getRect();
+    let closestX = constrain(rect.x, this.x - this.size / 2, this.x + this.size / 2);
+    let closestY = constrain(rect.y, this.y - this.size / 2, this.y + this.size / 2);
+    return dist(rect.x, rect.y, closestX, closestY) < this.size / 2;
+  }
 }
