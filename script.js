4009fae6203ab70eb7f49ecfa671b43f5f7b6c5d
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 12:04:34 +0100

    Introduce stars as background objects
    Added background stars to the game.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let player, enemy;
-let rocketImg, meteorImg, enemyImg; // Obrázky
+let rocketImg, meteorImg, enemyImg;
 let gravity = 0.5;
 let score = 0;
 let obstacles = [];
+let stars = [];
 
 function preload() {
   rocketImg = loadImage('images/rocketImg.png');
@@ -13,29 +14,27 @@ function preload() {
 function setup() {
   createCanvas(1240, 620);
   player = new Player();
-  // Tady pozor, pokud tvoje EnemyShip classa vyžaduje jiné parametry, uprav je:
   enemy = new EnemyShip(width + 1000, height / 2, enemyImg);
+  for (let i = 0; i < 50; i++) {
+    stars.push(new BackgroundObject());
+  }
 }
 
 function draw() {
   background(0);
-
+  for (let star of stars) {
+    star.update();
+    star.show();
+  }
   player.update();
   player.show();
-
   if (frameCount % 90 === 0) {
     obstacles.push(new Obstacle());
   }
-
   for (let i = obstacles.length - 1; i >= 0; i--) {
     obstacles[i].update();
     obstacles[i].show();
-
-    if (obstacles[i].hits(player)) {
-      noLoop();
-      console.log("Game Over - Meteor");
-    }
-
+    if (obstacles[i].hits(player)) noLoop();
     if (obstacles[i].offscreen()) {
       obstacles.splice(i, 1);
       score++;
@@ -43,18 +42,11 @@ function draw() {
       if (scoreEl) scoreEl.innerText = score;
     }
   }
-
   enemy.update();
   enemy.show();
-
-  if (enemy.checkCollision(player)) {
-    noLoop();
-    console.log("Game Over - Enemy");
-  }
+  if (enemy.checkCollision(player)) noLoop();
 }
 
 function keyPressed() {
-  if (key === ' ' || keyCode === 32) {
-    gravity *= -1;
-  }
+  if (key === ' ' || keyCode === 32) gravity *= -1;
 }
