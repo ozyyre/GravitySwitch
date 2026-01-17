f7db49682e9851326e742db058746cb82635f283
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 11:58:50 +0100

    Refactor Player class with new dimensions and methods

--- classes/Player.js
+++ classes/Player.js
@@ -1,36 +1,30 @@
 class Player {
-    constructor() {
-        this.size = 80;
-        this.x = 100;
-        this.y = height / 2;
-        this.vel = 0;
-    }
-    update() {
+  constructor() {
+    this.x = 64;
+    this.y = height / 2;
+    this.w = 50;
+    this.h = 50;
+    this.vel = 0; // Používáme tvůj název 'vel'
+  }
+
+  update() {
     this.vel += gravity;
-    this.y = constrain(this.y + this.vel, 0, height - this.h);
+    this.y += this.vel;
+    
+    // Ochrana proti vypadnutí (constrain)
+    this.y = constrain(this.y, 0, height - this.h);
+    
+    // Jemný odraz, jak jsi chtěl
     if (this.y <= 0 || this.y >= height - this.h) {
-        this.vel *= -0.2; 
+      this.vel *= -0.2;
     }
+  }
+
+  show() {
+    image(rocketImg, this.x, this.y, this.w, this.h);
+  }
+
+  getRect() {
+    return { x: this.x, y: this.y, w: this.w, h: this.h };
+  }
 }
-    }
-
-    show() {
-        push();
-        translate(this.x, this.y);
-        rotate(HALF_PI); 
-        imageMode(CENTER);
-        image(rocketImg, 0, 0, this.size, this.size);
-        pop();
-    }
-
-    getRect() {
-        return { x: this.x, y: this.y, w: this.size, h: this.size * 0.8 };
-    }
-}
-
-
-
-
-
-
-
