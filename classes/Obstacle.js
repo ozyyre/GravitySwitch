a2969d635f6710b46b0340ff4b0facef105144bf
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 12:04:04 +0100

    Refactor Obstacle class for improved readability

--- classes/Obstacle.js
+++ classes/Obstacle.js
@@ -2,18 +2,15 @@ class Obstacle {
   constructor() {
     this.size = random(30, 60);
     this.x = width + this.size;
-    if (random(1) < 0.5) {
-      this.y = random(this.size, height / 3);
-    } else {
-      this.y = random(2 * height / 3, height - this.size);
-    }
+    let isTop = random() < 0.5;
+    this.y = isTop ? random(this.size, height / 3) : random(2 * height / 3, height - this.size);
     this.speed = 5;
     this.angle = random(TWO_PI);
-    this.rotationSpeed = random(-0.05, 0.05);
+    this.rot = random(-0.05, 0.05);
   }
   update() {
-    this.x = this.x - this.speed;
-    this.angle = this.angle + this.rotationSpeed;
+    this.x -= this.speed;
+    this.angle += this.rot;
   }
   show() {
     push();
@@ -24,12 +21,12 @@ class Obstacle {
     pop();
   }
   offscreen() {
-    return this.x + this.size / 2 < 0;
+    return this.x < -this.size;
   }
   hits(player) {
-    let rect = player.getRect();
-    let closestX = constrain(rect.x, this.x - this.size / 2, this.x + this.size / 2);
-    let closestY = constrain(rect.y, this.y - this.size / 2, this.y + this.size / 2);
-    return dist(rect.x, rect.y, closestX, closestY) < this.size / 2;
+    let r = player.getRect();
+    let closestX = constrain(this.x, r.x, r.x + r.w);
+    let closestY = constrain(this.y, r.y, r.y + r.h);
+    return dist(this.x, this.y, closestX, closestY) < this.size / 2;
   }
 }
