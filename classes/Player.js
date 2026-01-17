067024fa6db682b44c647086e8ee50bcbf994f44
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 12:14:15 +0100

    Update Player.js

--- classes/Player.js
+++ classes/Player.js
@@ -1,23 +1,28 @@
 class Player {
-  constructor() {
-    this.x = 64;
-    this.y = height / 2;
-    this.w = 50;
-    this.h = 50;
-    this.vel = 0;
-  }
-  update() {
-    this.vel += gravity;
-    this.y += this.vel;
-    this.y = constrain(this.y, 0, height - this.h);
-    if (this.y <= 0 || this.y >= height - this.h) {
-      this.vel *= -0.2;
+    constructor() {
+        this.size = 80;
+        this.x = 100;
+        this.y = height / 2;
+        this.vel = 0;
     }
-  }
-  show() {
-    image(rocketImg, this.x, this.y, this.w, this.h);
-  }
-  getRect() {
-    return { x: this.x, y: this.y, w: this.w, h: this.h };
-  }
+
+    update() {
+        this.vel += gravity;
+        this.y = constrain(this.y + this.vel, this.size / 2, height - this.size / 2);
+        if (this.y === this.size/2 || this.y === height - this.size/2) this.vel = 0;
+    }
+
+    show() {
+        push();
+        translate(this.x, this.y);
+        rotate(HALF_PI); 
+        imageMode(CENTER);
+        image(rocketImg, 0, 0, this.size, this.size);
+        pop();
+    }
+
+    getRect() {
+        return { x: this.x, y: this.y, w: this.size, h: this.size * 0.8 };
+    }
 }
+
