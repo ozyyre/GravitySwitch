fbed43444985b70cb076269505fd8b4b9c09470f
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 12:03:47 +0100

    Update Player.js

--- classes/Player.js
+++ classes/Player.js
@@ -6,30 +6,18 @@ class Player {
     this.h = 50;
     this.vel = 0;
   }
-
   update() {
     this.vel += gravity;
     this.y += this.vel;
-    
-    // Omezení pohybu
     this.y = constrain(this.y, 0, height - this.h);
-    
-    // Odraz
     if (this.y <= 0 || this.y >= height - this.h) {
       this.vel *= -0.2;
     }
   }
-
   show() {
     image(rocketImg, this.x, this.y, this.w, this.h);
   }
-
   getRect() {
-    return {
-      x: this.x,
-      y: this.y,
-      w: this.w,
-      h: this.h
-    };
+    return { x: this.x, y: this.y, w: this.w, h: this.h };
   }
-} // <--- Tahle závorka tady musí být jako úplně poslední
+}
