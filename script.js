c57f869091638e88cb14bba27ceffcbfb01a4a42
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 11:46:59 +0100

    Simplify game code and improve structure
    Refactor game logic and remove unused variables.

--- script.js
+++ script.js
@@ -1,129 +1,66 @@
-let player;
-let enemy;
+let player, enemy, rocketImg, meteorImg, enemyImg;
 let gravity = 0.5;
 let score = 0;
-let bestScore = 0;
-
 let obstacles = [];
-let backgroundObjects = [];
-
-let rocketImg;
-let meteorImg;
-let enemyImg;
 
 function preload() {
   rocketImg = loadImage('images/rocketImg.png');
   meteorImg = loadImage('images/meteor.png');
   enemyImg = loadImage('images/enemyShip.png');
 }
-
 function setup() {
-  const container = document.querySelector('.container');
-  const topOffset = container ? container.offsetHeight + 20 : 80;
-
-  const cnv = createCanvas(windowWidth, windowHeight - topOffset);
-  cnv.style('display', 'block');
-
+  createCanvas(1240, 620);
   player = new Player();
   enemy = new EnemyShip(width + 1000, height / 2, enemyImg);
-
-  backgroundObjects = [];
-  for (let i = 0; i < 100; i++) {
-    backgroundObjects.push(new BackgroundObject());
-  }
-
-  const restartBtn = document.getElementById('restartBtn');
-  if (restartBtn) restartBtn.onclick = restartGame;
 }
-
 function draw() {
   background(0);
-
-  for (const bg of backgroundObjects) {
-    bg.update();
-    bg.show();
-  }
-
   player.update();
   player.show();
-
   if (frameCount % 90 === 0) {
     obstacles.push(new Obstacle());
   }
-
   for (let i = obstacles.length - 1; i >= 0; i--) {
-    const obs = obstacles[i];
-    obs.update();
-    obs.show();
-
-    if (obs.hits(player)) {
-      gameOver();
-      return;
+    obstacles[i].update();
+    obstacles[i].show();
+    if (obstacles[i].hits(player)) {
+      noLoop();
     }
-
-    if (obs.offscreen()) {
+    if (obstacles[i].offscreen()) {
       obstacles.splice(i, 1);
       score++;
-
-      const scoreEl = document.getElementById('score');
-      if (scoreEl) scoreEl.innerText = score;
-
-      if (enemy.x < -100 && random() < 0.15) {
-        enemy.x = width + 100;
-        enemy.y = random(height * 0.25, height * 0.75);
-      }
-
-      if (score % 10 === 0) {
-        obstacles.forEach(o => o.speed += 1);
-        enemy.speedX += 1;
-      }
+      document.getElementById('score').innerText = score;
     }
   }
-
   enemy.update();
   enemy.show();
 
   if (enemy.checkCollision(player)) {
-    gameOver();
+    noLoop();
   }
 }
-
 function keyPressed() {
-  if (keyCode === 32) {
+  if (key === ' ') {
     gravity *= -1;
   }
 }
-
-function windowResized() {
-  const container = document.querySelector('.container');
-  const topOffset = container ? container.offsetHeight + 20 : 80;
-  resizeCanvas(windowWidth, windowHeight - topOffset);
-}
-
-function restartGame() {
-  score = 0;
-  gravity = 0.5;
-  obstacles = [];
-
-  player = new Player();
-  enemy = new EnemyShip(width + 1000, height / 2, enemyImg);
-
-  const scoreEl = document.getElementById('score');
-  if (scoreEl) scoreEl.innerText = score;
-
-  loop();
-}
-
-function gameOver() {
-  noLoop();
-
-  if (score > bestScore) {
-    bestScore = score;
+class Player {
+  constructor() {
+    this.x = 64;
+    this.y = height / 2;
+    this.w = 50;
+    this.h = 50;
+    this.velocity = 0;
   }
-
-  const bestEl = document.getElementById('bestScore');
-  const scoreEl = document.getElementById('score');
-
-  if (bestEl) bestEl.innerText = bestScore;
-  if (scoreEl) scoreEl.innerText = `Game Over! Skóre: ${score}`;
+  update() {
+    this.velocity += gravity;
+    this.y += this.velocity;
+    this.y = constrain(this.y, 0, height - this.h);
+    if (this.y === 0 || this.y === height - this.h) {
+      this.velocity = 0;
+    }
+  }
+  show() {
+    image(rocketImg, this.x, this.y, this.w, this.h);
+  }
 }
