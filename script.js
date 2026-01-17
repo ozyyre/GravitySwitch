0cdf19a24a814f022720060e8c52e3c383159b56
Ondřej Tesařík <ondrej.tesarik@gmail.com>
2026-01-17 11:55:02 +0100

    Refactor player and enemy initialization in script.js

--- script.js
+++ script.js
@@ -8,30 +8,40 @@ function preload() {
   meteorImg = loadImage('images/meteor.png');
   enemyImg = loadImage('images/enemyShip.png');
 }
+
 function setup() {
   createCanvas(1240, 620);
+  // Vyvolání tříd (recepty jsou v jiných souborech)
   player = new Player();
   enemy = new EnemyShip(width + 1000, height / 2, enemyImg);
 }
+
 function draw() {
   background(0);
+
   player.update();
   player.show();
+
   if (frameCount % 90 === 0) {
     obstacles.push(new Obstacle());
   }
+
   for (let i = obstacles.length - 1; i >= 0; i--) {
     obstacles[i].update();
     obstacles[i].show();
+
     if (obstacles[i].hits(player)) {
       noLoop();
     }
+
     if (obstacles[i].offscreen()) {
       obstacles.splice(i, 1);
       score++;
-      document.getElementById('score').innerText = score;
+      let scoreEl = document.getElementById('score');
+      if (scoreEl) scoreEl.innerText = score;
     }
   }
+
   enemy.update();
   enemy.show();
 
@@ -39,28 +49,9 @@ function draw() {
     noLoop();
   }
 }
+
 function keyPressed() {
   if (key === ' ') {
     gravity *= -1;
-  }
-}
-class Player {
-  constructor() {
-    this.x = 64;
-    this.y = height / 2;
-    this.w = 50;
-    this.h = 50;
-    this.velocity = 0;
-  }
-  update() {
-    this.velocity += gravity;
-    this.y += this.velocity;
-    this.y = constrain(this.y, 0, height - this.h);
-    if (this.y === 0 || this.y === height - this.h) {
-      this.velocity = 0;
-    }
-  }
-  show() {
-    image(rocketImg, this.x, this.y, this.w, this.h);
   }
 }
